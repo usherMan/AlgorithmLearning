76c4ba40a7bf6455ea6bd378c90b5f82ddfdd34b
usherman <1461148568@qq.com>
2019-08-14 19:32:59 +0800

    1、测试

--- AlgorithmLearning/ViewController.m
+++ AlgorithmLearning/ViewController.m
@@ -17,6 +17,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSMutableArray *array = [NSMutableArray arrayWithArray:@[@"23",@"265",@"89",@"11"]];
+    [self bubbleDescendingSort:array];
+}
+
+/**
+ 冒泡降序排序
+
+ @param array 待排序数据
+ */
+- (NSMutableArray *)bubbleDescendingSort:(NSMutableArray *)array{
+    
+    NSNumber *temp;
+    for(int i=0; i < array.count -1; i++){
+        for (int j = i+1; j < array.count-1-i; j++) {
+            if ([array[j] intValue] < [array[j+1] intValue]) {
+                temp = array[j] ;
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    NSLog(@"冒泡降序结果：%@",array);
+    return array;
 }
 
 
